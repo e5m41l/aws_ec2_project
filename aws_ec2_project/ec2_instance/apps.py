150f846449ed05748f9457e691995af0f75e8b02
Mostafa Esmail <100853809+e5m41l@users.noreply.github.com>
2024-12-23 16:04:04 +0200

    Delete aws_ec2_project directory

--- aws_ec2_project/ec2_instance/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Ec2InstanceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ec2_instance'
--- aws_ec2_project/ec2_instance/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class EC2InstanceSerializer(serializers.Serializer):
-    cpu = serializers.IntegerField(required=True)  # Number of vCPUs
-    ram = serializers.IntegerField(required=True)  # RAM in GB
-
